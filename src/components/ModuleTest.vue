58e5c445860ad7939cd9d71d3be84a002f2c9455
Andrej Neprokin <a.neprokin@gmail.com>
2025-06-22 01:08:50 +0100

    Updated ModuleTest.vue

--- src/components/ModuleTest.vue
+++ src/components/ModuleTest.vue
@@ -46,8 +46,8 @@
       <!-- Progress Bar -->
       <div class="mb-6">
         <div class="flex justify-between text-sm text-gray-600 mb-2">
-          <span v-if="currentQuestion <= questions.length">
-            Question {{ currentQuestion + 1 }} offf {{ questions.length }}
+          <span v-if="currentQuestion != questions.length">
+            Question {{ currentQuestion + 1 }} of {{ questions.length }}
           </span>
           <span>{{ Math.round((currentQuestion / questions.length) * 100) }}%</span>
         </div>
