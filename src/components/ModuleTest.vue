ff423a467210414c4b73611a3e8b26493d62efe5
Andrej Neprokin <a.neprokin@gmail.com>
2025-06-22 01:01:57 +0100

    Updated ModuleTest.vue

--- src/components/ModuleTest.vue
+++ src/components/ModuleTest.vue
@@ -52,7 +52,7 @@
         <div class="w-full bg-gray-200 rounded-full h-2">
           <div
             class="bg-yellow-400 h-2 rounded-full transition-all duration-300"
-            :style="{ width: ((currentQuestion + 1) / questions.length) * 100 + '%' }"
+            :style="{ width: (currentQuestion / questions.length) * 100 + '%' }"
           ></div>
         </div>
       </div>
