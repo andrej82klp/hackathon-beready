54527625341a9839b74659470cb24bf91ff4c24d
Andrej Neprokin <a.neprokin@gmail.com>
2025-06-26 13:57:50 +0100

    Updated Donate.vue

--- src/pages/Donate.vue
+++ src/pages/Donate.vue
@@ -220,7 +220,7 @@
                   <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
                 </svg>
               </div>
-              <div class="ml-3">
+              <<div class="ml-3">
                 <h3 class="text-sm font-medium text-green-800">
                   Your donation is secure and tax-deductible
                 </h3>
